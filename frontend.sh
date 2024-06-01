8bf4b42ec11e246aa4ac47ae9592389f6396dafa
sai <kuppalasaivikram@gmail.com>
2024-06-01 06:38:44 +0530

    updated code for frontend script

--- frontend.sh
+++ frontend.sh
@@ -1,14 +1,10 @@
 dnf install nginx -y
 systemctl enable nginx
-systemctl restart nginx
-
+systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 
-curl -o /tmp/frontend.zip
-
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
 cd /usr/share/nginx/html
-unzip frontend.zip
-vim /etc/nginx/default.d/expense.conf
-
+unzip /tmp/frontend.zip
+# Copy file step
 systemctl restart nginx
-
