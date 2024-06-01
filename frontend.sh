d2c1e656cff4d146ab78108761c16a300a83b41d
sai <kuppalasaivikram@gmail.com>
2024-06-01 15:17:06 +0530

    introduced backend service and mysql

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,13 @@
 dnf install nginx -y
-systemctl enable nginx
+# here it placed because we are switching directory at end
+cp expense.conf /etc/nginx/default.d/expense.conf
+
 systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
-cd /usr/share/nginx/html
+cd  /usr/share/nginx/html
 unzip /tmp/frontend.zip
-# Copy file step
+
 systemctl restart nginx
+systemctl enable nginx
